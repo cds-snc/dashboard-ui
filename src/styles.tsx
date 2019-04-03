6d4121fcc8474807eff0e6777d3b93ec12d57dec
nmakuch <makuch.nick@gmail.com>
2019-04-02 20:01:46 -0400

    AWS Widget completed

--- src/styles.tsx
+++ src/styles.tsx
@@ -1,13 +1,14 @@
 export function getStyles() {
     const WHITE_COLOR = "#FFFFFF";
     const BLACK_COLOR = "#000000"
-    const BLUE_COLOR = "#00a3de";
+    const BLUE_COLOR = "#367BBC";
     const RED_COLOR = "#7c270b";
     const AXIS_COLOR = "#FFFFFF";
     const LIGHTGRAY_COLOR = "#f0efef";
     const BLACKDARK_COLOR = "#1A1B1E";
     const BLACKLIGHT_COLOR = "#292A29";
     const GREEN_COLOR = "#31D397";
+
 
     return {
       title: {
@@ -37,11 +38,9 @@ export function getStyles() {
         }
       },
 
-      // DATA SET ONE
+      // DEPENDANT AXIS
       axisOne: {
-        grid: {
-          strokeWidth: 0
-        },
+        grid: { strokeWidth: 0 },
         axis: { stroke: AXIS_COLOR, strokeWidth: 1 }, 
         ticks: { stroke: AXIS_COLOR , strokeWidth: 1 },
         tickLabels: {
@@ -51,9 +50,26 @@ export function getStyles() {
         }
       },
 
+      axisTwo: {
+        grid: { strokeWidth: 0 },
+        axis: { stroke: AXIS_COLOR, strokeWidth: 1 }, 
+        ticks: { stroke: AXIS_COLOR , strokeWidth: 1 },
+        tickLabels: {
+          fill: AXIS_COLOR,
+          fontFamily: "inherit",
+          fontSize: "10px",
+        }
+      },
+
       // HEROKU WIDGET BAR STYLES
       herokuBar: {
         data: { fill: GREEN_COLOR },
+        labels: { fill: WHITE_COLOR, fontSize: "12px" }
+      },
+
+      // AWS WIDGET BAR STYLES
+      AWSBar: {
+        data: { fill: BLUE_COLOR },
         labels: { fill: WHITE_COLOR, fontSize: "12px" }
       }
     };
