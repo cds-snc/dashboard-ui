bfd63f0e4429c0cffa9d1ce68eb01f1c0d2369fd
smcmurtry <smcmurtry@gmail.com>
2019-04-16 16:17:53 -0400

    deleted comments

--- stage-server.js
+++ stage-server.js
@@ -1,11 +1,9 @@
 const port = parseInt(process.env.PORT, 10) || 4000;
-// import path from "path";
-// import express from "express";
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 
 const server = express();
-server.use('/', express.static(path.join(__dirname, "build/")));
+server.use("/", express.static(path.join(__dirname, "build/")));
 server.listen(port, err => {
     if (err) throw err;
     console.log(`> Ready on http://localhost:${port}`);
