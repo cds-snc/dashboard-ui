a3e4fb2c5041a3fff7dd1557a82d294ff3abdb18
nmakuch <makuch.nick@gmail.com>
2019-04-18 13:50:38 -0400

    added phase badge to the page header

--- src/widgets/PageHeader.tsx
+++ src/widgets/PageHeader.tsx
@@ -28,15 +28,39 @@ h1 {
   color: white;
   margin: 0;
   line-height: 1.2rem;
-  margin-bottom: 1.4rem;
 }
 `;
+
+const phaseBadge = css`
+  background: #f90277;
+  font-size: 1.2rem;
+  border-radius: 0.5rem;
+  color: white;
+  font-weight: 700;
+  padding: 0.2rem 0.8rem;
+  margin-left: 1rem;
+`
+
+const titleContainer = css`
+  display: flex;
+  margin-bottom: 1rem;
+  align-items: center;
+`
+
+const PhaseBadge = () => {
+    return (
+      <span css={phaseBadge}>Alpha</span>
+    );
+  };
 
 const PageHeader = () => {
   return (
     <div css={header}>
         <div>
-            <h1>Loon Dashboard UI (alpha banner)</h1>
+            <div css={titleContainer}>
+                <h1>Loon Dashboard UI</h1>
+                <PhaseBadge />
+            </div>
             <nav css={navStyle}>
                 <Link to="/">Home</Link>
                 &nbsp; | &nbsp;
