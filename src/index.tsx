6a3b4adadf2a49bf7d38cbea29b832406e3186e8
Stephen McMurtry <smcmurtry@gmail.com>
2019-04-23 09:47:48 -0400

    Fix routing bug (#31)

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 /** @jsx jsx */ jsx;
 import { jsx, css } from "@emotion/core";
 import React from "react";
-import ReactDOM from "react-dom";
+import { hydrate, render } from "react-dom";
 import PageHeader from './widgets/PageHeader';
 import "./index.css";
 import Cost from "./Cost";
 import Vac from "./Vac";
 import Home from "./Home";
 import * as serviceWorker from "./serviceWorker";
+
 import { Router } from "@reach/router";
 
 const App = () => (
@@ -15,13 +16,18 @@ const App = () => (
     <PageHeader />
     <Router>
       <Home path="/" />
-      <Cost path="/cost" />
-      <Vac path="/vac" />
+      <Cost path="cost/" />
+      <Vac path="vac/" />
     </Router>
   </div>
 );
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const rootElement = document.getElementById("root");
+if (rootElement && rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
